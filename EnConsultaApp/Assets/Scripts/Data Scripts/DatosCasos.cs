735ede3f29cb3b4b64d43c73cc0ae9772818f5c5
Sebastian Gil Parga <s.gilparga@gmail.com>
2014-10-22 14:10:43 -0500

    Carga de los datos de la consulta completa

---
+++ EnConsultaApp/Assets/Scripts/Data Scripts/DatosCasos.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DatosCasos : MonoBehaviour {
+
+	//------------------------------------------------------------------------------------
+	// Atributos
+	//------------------------------------------------------------------------------------
+
+	// Contiene los distintos parrafos de informacion que hacer parte del motivo de la
+	// consulta expuesta por el paciente
+	public	List<string>					motivoConsulta;
+
+	// Contiene una llave pregunta-respuesta con las preguntas que el estudiante puede
+	// hacer para conocer informacion de la enfermedad actual
+	public	Dictionary<string,string>		enfermedadActual;
+
+	// Contiene una llave pregunta-respuesta con las preguntas que el estudiante puede
+	// hacer para conocer los antecedentes del paciente
+	public	Dictionary<string,string>		antecedentes;
+
+	// Contiene una llave pregunta-respuesta con las preguntas que debe y no debe hacer
+	// para conocer informacion adicional del paciente
+	public	Dictionary<string, string>		infoAdicional;
+
+	// Contiene una llave pregunta-valor,razon con la relevancia que el profesor da a cada una
+	// de las preguntas de la infromacion adicional
+	public	Dictionary<string, string[]>	preguntasCalificacion;
+
+	// Contiene una llave examen-resultado con los examenes dispuestos por el profesor
+	// y el resultado que estos arrojan
+	public	Dictionary<string, string>		resultadosExamenes;
+
+	// Contienen una llave examen-valor,razon con los examenes dispuestos en la lista anterior
+	// y la relevancia que el profesor dispone de cada uno de ellos
+	public	Dictionary<string, string[]>	examenesCalificacion;
+
+	//---------------------------------------------------------------------------------------
+	// Constructor
+	//---------------------------------------------------------------------------------------
+
+	// No permite que el objeto se destruya al cambiar la escena
+	void Awake(){
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	void Start () {
+		motivoConsulta = new List<string>();
+		enfermedadActual = new Dictionary<string,string>();
+		antecedentes = new Dictionary<string, string>();
+		infoAdicional = new Dictionary<string, string>();
+		preguntasCalificacion = new Dictionary<string, string[]>();
+		resultadosExamenes = new Dictionary<string, string>();
+		examenesCalificacion = new Dictionary<string, string[]>();
+
+		Inicializar();
+	}
+
+	//--------------------------------------------------------------------------------------
+	// Metodos
+	//--------------------------------------------------------------------------------------
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Inicializar(){
+		Debug.Log("iniciando");
+
+		// Carga motivoConsulta
+		TextAsset request = (TextAsset)Resources.Load("motivoConsulta", typeof(TextAsset));
+		string[] data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			motivoConsulta.Add(data[i]);
+		}
+
+		//carga enfermedad actual
+		request = (TextAsset)Resources.Load("enfermedadActual", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] dupla = data[i].Split(';');
+			enfermedadActual.Add(dupla[0],dupla[1]);
+		}
+
+		//carga antecedentes
+		request = (TextAsset)Resources.Load("antecedentes", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] dupla = data[i].Split(';');
+			antecedentes.Add(dupla[0],dupla[1]);
+		}
+
+		//carga infoAdicional
+		request = (TextAsset)Resources.Load("infoAdicional", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] dupla = data[i].Split(';');
+			infoAdicional.Add(dupla[0],dupla[1]);
+		}
+
+		//carga preguntasCalificacion
+		request = (TextAsset)Resources.Load("preguntasCalificacion", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] tripleta = data[i].Split(';');
+			string[] valor = new string[2];
+			valor[0] = tripleta[1];
+			valor[1] = tripleta[2];
+			preguntasCalificacion.Add(tripleta[0],valor);
+		}
+	
+
+		//carga resultadosExamenes
+		request = (TextAsset)Resources.Load("resultadosExamenes", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] dupla = data[i].Split(';');
+			resultadosExamenes.Add(dupla[0],dupla[1]);
+		}
+
+		//carga examenesCalificacion
+		request = (TextAsset)Resources.Load("examenesCalificacion", typeof(TextAsset));
+		data = request.text.Split('\n');
+		for(int i=0;i < data.Length;i++){
+			string[] tripleta = data[i].Split(';');
+			string[] valor = new string[2];
+			valor[0] = tripleta[1];
+			valor[1] = tripleta[2];
+			examenesCalificacion.Add(tripleta[0],valor);
+		}
+	}
+}
