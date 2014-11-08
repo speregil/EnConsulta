f89ae76b9225960aad1e8949986b7b7738084794
Sebastian Gil Parga <s.gilparga@gmail.com>
2014-11-07 19:45:18 -0500

    Ya es posible navegar los examenes disponibles

--- EnConsultaApp/Assets/Scripts/Manager Scripts/ExamenFisicoManager.cs
+++ EnConsultaApp/Assets/Scripts/Manager Scripts/ExamenFisicoManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 /*
  * Controla las acciones principales del modulo de examen fisico
@@ -11,7 +12,13 @@ public class ExamenFisicoManager : MonoBehaviour {
 	private MainManager		mm;
 	private DatosCasos		dc;
 	private DatosEstudiante	de;
+	
 	public	string 			IDModulo;
+	public	GameObject[]	examenes;
+	public	GameObject		btnSig;
+	public	GameObject		btnAnt;
+	public	int				nextIndex;
+	public 	int				prevIndex;
 
 	//---------------------------------------------------------------
 	// Constructor
@@ -19,17 +26,79 @@ public class ExamenFisicoManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		IDModulo = "ModuloExamenFisico";
 		GameObject dm = GameObject.Find("DataManager");
 		mm = (MainManager)dm.GetComponent(typeof(MainManager));
 		dc = (DatosCasos)dm.GetComponent(typeof(DatosCasos));
 		de = (DatosEstudiante)dm.GetComponent(typeof(DatosEstudiante));
 
-		Debug.Log(dc.resultadosExamenes.Values);
+		prevIndex = 0;
+		nextIndex = MostrarExamenes(0);
+
+		if(nextIndex != 0)
+			btnSig.SetActive(true);
 	}
 
 	//---------------------------------------------------------------
 	// Metodos
 	//---------------------------------------------------------------
+
+	public int MostrarExamenes(int index){
+		bool acabo = false;
+		int orIndex = index;
+		int exIndex = 0;
+	    string[] keys = new string[dc.resultadosExamenes.Keys.Count];
+		dc.resultadosExamenes.Keys.CopyTo(keys, 0);
+		for(int i = index; i < keys.Length && !acabo; i++){
+			if(exIndex < examenes.Length){
+				GameObject toggle = examenes[exIndex];
+				Text txt = (Text)toggle.GetComponent(typeof(Text));
+				txt.text = keys[i];
+				exIndex++;
+			}
+			else{
+				index = i;
+				acabo = true;
+			}
+		}
+
+		if(acabo){
+			prevIndex = orIndex;	
+			return index;
+		}
+
+		return 0;
+	}
+
+	public void LimpiarPagina(){
+		for(int i = 0; i < examenes.Length; i++){
+			GameObject toggle = examenes[i];
+			Text txt = (Text)toggle.GetComponent(typeof(Text));
+			txt.text = "";
+		}
+	}
+
+	public void AvanzarPagina(){
+
+		LimpiarPagina();
+
+		btnAnt.SetActive(true);
+		nextIndex = MostrarExamenes(nextIndex);
+
+		if(nextIndex == 0)
+			btnSig.SetActive(false);
+	}
+
+	public void RetrocederPagina(){
+
+		LimpiarPagina();
+
+		btnSig.SetActive(true);
+		nextIndex = MostrarExamenes(prevIndex);
+
+		if(prevIndex == 0)
+			btnAnt.SetActive(false);
+	}
 
 	// Update is called once per frame
 	void Update () {
