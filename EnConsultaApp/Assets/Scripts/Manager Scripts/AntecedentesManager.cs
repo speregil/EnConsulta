843d18171aaaa153b808b11efd5457eed4ca8961
Jaime <jaimepinedamartinez@gmail.com>
2014-12-03 18:02:11 -0500

    Ya esta funcionando modulo antecedentes

--- EnConsultaApp/Assets/Scripts/Manager Scripts/AntecedentesManager.cs
+++ EnConsultaApp/Assets/Scripts/Manager Scripts/AntecedentesManager.cs
@@ -17,6 +17,10 @@ public class AntecedentesManager : MonoBehaviour {
 	public 	GameObject  	panelNarracion;
 	public 	GameObject  	panelPreguntas;
 	public 	GameObject  	panelRespuestas;
+
+	public	GameObject[]	examenes;
+	public	GameObject[]	respuestas;
+
 	// Identificador del modulo = ModuloExamenFisico
 
 
@@ -45,7 +49,43 @@ public class AntecedentesManager : MonoBehaviour {
 	}
 
 	public void MostrarPreguntas(){
+		bool acabo = false;
+		int orIndex = 0;
+		int exIndex = 0;
 		panelPreguntas.SetActive(true);
+
+		string[] keys = new string[dc.antecedentes.Keys.Count];
+		dc.antecedentes.Keys.CopyTo(keys, 0);
+		// Itera para dibujar el nombre de cada examens
+		for(int i = 0; i < keys.Length && !acabo; i++){
+			//Itera en la lista de examenes hasta que se acaban los labels diponebles en el panel
+			if(exIndex < examenes.Length){
+				GameObject button = examenes[exIndex];
+				GameObject text = button.transform.GetChild(0).gameObject;
+				Text txt = (Text)text.GetComponent(typeof(Text));
+				txt.text = keys[i];
+				
+				Button go = (Button)button.GetComponent(typeof(Button));
+				go.interactable = true;
+				// Determina si el examen ya habia sido seleccionado previamente y lo deja seleccionado
+
+				
+				exIndex++;
+			}
+			else{
+				//index = i;
+				acabo = true;
+			}
+		}
+		
+		if(acabo){
+			//Retorna el index en el que se debe empezar a dibujar si faltan examenes en la lista
+			//indxAnteriores[prevIndex] = orIndex;
+			//return index;
+		}
+		// Retorna 0 si la lista se termino de dibujar
+		//return 0;
+
 		
 		
 	}
@@ -54,9 +94,48 @@ public class AntecedentesManager : MonoBehaviour {
 		
 	}
 
-	public void MostrarRespuestas(){
+	public void MostrarRespuestas(int pos){
+	
 		panelRespuestas.SetActive(true);
+		bool acabo = false;
+		int orIndex = 0;
+		int exIndex = 0;
+		panelPreguntas.SetActive(true);
 		
+		string[] values = new string[dc.antecedentes.Values.Count];
+		dc.antecedentes.Values.CopyTo(values, 0);
+		// Itera para dibujar el nombre de cada examens
+		for(int i = 0; i < values.Length && !acabo; i++){
+			//Itera en la lista de examenes hasta que se acaban los labels diponebles en el panel
+			if(exIndex < respuestas.Length){
+				GameObject text = respuestas[exIndex];			
+				Text txt = (Text)text.GetComponent(typeof(Text));
+				txt.text = values[pos];
+				exIndex++;
+				text = respuestas[exIndex];			
+			    txt = (Text)text.GetComponent(typeof(Text));
+				txt.text = values[pos];
+
+				// Determina si el examen ya habia sido seleccionado previamente y lo deja seleccionado
+				
+				
+				exIndex++;
+			}
+			else{
+				//index = i;
+				acabo = true;
+			}
+		}
+		
+		if(acabo){
+			//Retorna el index en el que se debe empezar a dibujar si faltan examenes en la lista
+			//indxAnteriores[prevIndex] = orIndex;
+			//return index;
+		}
+		// Retorna 0 si la lista se termino de dibujar
+		//return 0;
+
+
 		
 	}
 	public void QuitarRespuestas(){
